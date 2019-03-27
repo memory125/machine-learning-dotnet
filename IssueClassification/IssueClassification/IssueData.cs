76bdc14f736e403f066f453ca5f870e1428df700
De Meng <mengde1120@sina.com>
2019-03-27 11:00:10 +0800

    add issue classification sample

---
+++ IssueClassification/IssueClassification/IssueData.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.ML.Data;
+
+namespace IssueClassification
+{
+    public class Issue
+    {
+        [LoadColumn(0)]
+        public string ID { get; set; }
+        [LoadColumn(1)]
+        public string Area { get; set; }
+        [LoadColumn(2)]
+        public string Title { get; set; }
+        [LoadColumn(3)]
+        public string Description { get; set; }
+    }
+
+    public class IssuePrediction
+    {
+        [ColumnName("PredictedLabel")]
+        public string Area;
+    }
+}
--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Machine Learning Dotnet
 * This repository is focus on machine learning sample via ML.net.
-
+* This repository includes the following samples.
+  * Sentiment Anaysis
+  * Issue Classification
+  
 ## Sentiment Analysis
 * This sample includes the following operations.
   * Prepare data
